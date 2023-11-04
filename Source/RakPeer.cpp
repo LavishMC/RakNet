793980cdc3a45fb3558b0dac21a4e325bdd7bc0a
MisledWater079 <59891258+Misledwater79@users.noreply.github.com>
2023-11-04 17:08:15 -0400

    Updated for Minecraft
    Changed to work with Minecraft. Credit goes to vp817 from AllayBE.

--- Source/RakPeer.cpp
+++ Source/RakPeer.cpp
@@ -2271,10 +2271,11 @@ void RakPeer::SetOfflinePingResponse( const char *data, const unsigned int lengt
 	RakAssert(length < 400);
 
 	rakPeerMutexes[ offlinePingResponse_Mutex ].Lock();
-	offlinePingResponse.Reset();
+	//offlinePingResponse.Reset();
 
-	if ( data && length > 0 )
-		offlinePingResponse.Write( data, length );
+	//if ( data && length > 0 )
+	//	offlinePingResponse.Write( data, length );
+	offlinePingResponse = data;
 
 	rakPeerMutexes[ offlinePingResponse_Mutex ].Unlock();
 }
@@ -2288,8 +2289,8 @@ void RakPeer::SetOfflinePingResponse( const char *data, const unsigned int lengt
 void RakPeer::GetOfflinePingResponse( char **data, unsigned int *length )
 {
 	rakPeerMutexes[ offlinePingResponse_Mutex ].Lock();
-	*data = (char*) offlinePingResponse.GetData();
-	*length = (int) offlinePingResponse.GetNumberOfBytesUsed();
+	*data = (char*) offlinePingResponse;
+	//*length = (int) offlinePingResponse.GetNumberOfBytesUsed();
 	rakPeerMutexes[ offlinePingResponse_Mutex ].Unlock();
 }
 
@@ -4654,7 +4655,7 @@ bool ProcessOfflineNetworkPacket( SystemAddress systemAddress, const char *data,
 
 				rakPeer->rakPeerMutexes[ RakPeer::offlinePingResponse_Mutex ].Lock();
 				// They are connected, so append offline ping data
-				outBitStream.Write( (char*)rakPeer->offlinePingResponse.GetData(), rakPeer->offlinePingResponse.GetNumberOfBytesUsed() );
+				outBitStream.Write( (char*)rakPeer->offlinePingResponse);
 				rakPeer->rakPeerMutexes[ RakPeer::offlinePingResponse_Mutex ].Unlock();
 
 				unsigned i;
--- Source/RakPeer.h
+++ Source/RakPeer.h
@@ -764,7 +764,7 @@ protected:
 	//unsigned short remoteSystemListSize;
 	///Store the maximum incoming connection allowed 
 	unsigned int maximumIncomingConnections;
-	RakNet::BitStream offlinePingResponse;
+	const char* offlinePingResponse;
 	///Local Player ID
 	// SystemAddress mySystemAddress[MAXIMUM_NUMBER_OF_INTERNAL_IDS];
 	char incomingPassword[256];
